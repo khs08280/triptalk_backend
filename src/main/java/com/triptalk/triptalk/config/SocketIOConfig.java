a76d227e98a2d3ad7252c49df84e8456eab92909
khs08280 <u6959@naver.com>
2025-03-03 14:42:21 +0900

    .

--- src/main/java/com/triptalk/triptalk/config/SocketIOConfig.java
+++ src/main/java/com/triptalk/triptalk/config/SocketIOConfig.java
@@ -24,33 +24,12 @@ public class SocketIOConfig {
 
   @Value("${socket.io.port}")
   private int port;
-//
-//  @Value("${server.ssl.key-store}")
-//  private String keystorePath;
-//
-//  @Value("${server.ssl.key-store-password}")
-//  private String keystorePassword;
-//
-//  @Value("${socket.io.keystore-path:/app/localhost.p12}")
-//  private String keystoreFilePath;
 
   @Bean
   public SocketIOServer socketIOServer() throws Exception {
     Configuration config = new Configuration();
     config.setHostname(host);
     config.setPort(port);
-
-//    String path = ClassLoader.getSystemResource("localhost.p12").getPath();
-//    FileInputStream fileInputStream = new FileInputStream(path);
-
-//    org.springframework.core.io.Resource resource =
-//            new org.springframework.core.io.ClassPathResource("localhost.p12");
-//    File keyStoreFile = resource.getFile();
-//    File keyStoreFile = new File(keystoreFilePath);
-//    FileInputStream fileInputStream = new FileInputStream(keyStoreFile);
-//
-//    config.setKeyStorePassword(keystorePassword);
-//    config.setKeyStore(fileInputStream);
 
     config.getAllowHeaders();
     config.setOrigin("https://triptalk1.netlify.app");
